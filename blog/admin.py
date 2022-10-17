ec59eabac1514e3118bd3aa6039e6628003eda98
James Latten <james@sitelyftstudios.com>
2022-10-16 21:56:51 -0400

    This will update the database

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Jobs
 
 # Post admin settings
 class PostAdmin(admin.ModelAdmin):
@@ -8,5 +8,13 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title', )}
 
+# Jobs Admin Settings
+class JobsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title', )}
+
 # Register your models here.
 admin.site.register(Post, PostAdmin)
+admin.site.register(Jobs)
--- blog/models.py
+++ blog/models.py
@@ -18,9 +18,22 @@ class Post(models.Model):
     created_on=models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
-
     class Meta:
         ordering = ['-created_on']
     
     def __str__(self):
         return self.title
+
+# Jobs Database Model
+class Jobs(models.Model):
+    job_title = models.CharField(max_length=255)
+    job_location = models.CharField(max_length=255)
+    job_desc = models.TextField()
+    job_date = models.CharField(max_length=200)
+    job_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-job_added']
+
+    def __str__(self):
+        return self.job_title
